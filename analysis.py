9a51c29c14c3a3f5f1ae438310859a9c7baef018
Bingbong0201 <jonnathan.zunalargo@gmail.com>
2022-10-26 19:50:43 -0400

    Updated Readme

--- analysis.py
+++ analysis.py
@@ -26,6 +26,18 @@ def get_lowest_gdp(data, year):
         new_num = num - 1
     state = data[new_num]["GeoName"]
     return state
+
+def get_state_gdp(data, state, year):
+  num_item = 0
+  for i in data: 
+    gdpstate = i['GeoName']
+    num_item += 1
+    if gdpstate == state:
+      new_num = num_item
+  gdp = data[new_num - 1][year]
+  print(gdp)
+  
+
 # save each row into a list (TODO: change to your path!)
 list_data = []
 with open("state_gdp_analysis.csv", "r") as infile:
@@ -38,6 +50,7 @@ with open("state_gdp_analysis.csv", "r") as infile:
 # get highest gdp for 2020 using "get_highest_gdp(list_data, '2020')"
 print(get_highest_gdp(list_data, '2020'))
 
-#print(list_data[1]["GeoName"])
 # get lowest gdp for 2020 using "get_lowest_gdp(list_data, '2020')"
 print(get_lowest_gdp(list_data, '2020'))
+
+get_state_gdp(list_data, "New York", "2019")
