5134525891cdbdf56ca9999eaf1f2a66c73d2e25
Bingbong0201 <jonnathan.zunalargo@gmail.com>
2022-10-26 18:17:43 -0400

    final solution

--- analysis.py
+++ analysis.py
@@ -4,20 +4,28 @@ from numpy import number
 
 def get_highest_gdp(data, year):
     highest_gdp = 0
+    num = 0
     for i in data:
       number = float(i[year])
+      num += 1
       if number > highest_gdp:
         highest_gdp = number
-    return highest_gdp
+        new_num = num - 1
+    state = data[new_num]["GeoName"]
+    return state
+  
 
 def get_lowest_gdp(data, year):
-    lowest_gdp = 0
+    lowest_gdp = float(data[1][year])
+    num = 0
     for i in data:
       number = float(i[year])
+      num += 1
       if number < lowest_gdp:
         lowest_gdp = number
-    return lowest_gdp  
-
+        new_num = num - 1
+    state = data[new_num]["GeoName"]
+    return state
 # save each row into a list (TODO: change to your path!)
 list_data = []
 with open("state_gdp_analysis.csv", "r") as infile:
@@ -30,5 +38,6 @@ with open("state_gdp_analysis.csv", "r") as infile:
 # get highest gdp for 2020 using "get_highest_gdp(list_data, '2020')"
 print(get_highest_gdp(list_data, '2020'))
 
+#print(list_data[1]["GeoName"])
 # get lowest gdp for 2020 using "get_lowest_gdp(list_data, '2020')"
 print(get_lowest_gdp(list_data, '2020'))
